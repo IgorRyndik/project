c356d1d50a18c8494cb670d7b4d5bd5d29dd44c8
Igor-Net <igorryndik@yandex.ua>
2023-10-02 18:38:30 +0300

    load from json

---
+++ src/load_dataset_from_json.py
@@ -0,0 +1,23 @@
+import json
+import numpy as np
+
+def load_dataset_from_json(json_filename):
+    """
+    Load dataset (data and labels) from a JSON file.
+
+    Args:
+    - json_filename (str): The filename of the JSON file.
+
+    Returns:
+    - data (numpy.ndarray): The loaded data.
+    - labels (numpy.ndarray): The loaded labels.
+    """
+    with open(json_filename, "r") as json_file:
+        dataset_dict = json.load(json_file)
+
+    # Convert the lists in the dictionary back to NumPy arrays
+    data = np.array(dataset_dict["data"])
+    labels = np.array(dataset_dict["labels"])
+
+    return data, labels
+
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 
 from dataset_load import load_dataset
 from save_dataset_to_json import save_dataset_to_json
+from load_dataset_from_json import load_dataset_from_json
 
 # Get the current directory of the Python script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -9,8 +10,11 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 # Specify the relative path to the dataset directory
 dataset_dir = os.path.join(script_dir, '..', 'data')
 
+json_filename = "project\data.json"
+
 # need to execute only on the first run
-# json_filename = "project\data.json"
 
 # mfcc, labels = load_dataset(dataset_dir)
 # save_dataset_to_json(mfcc, labels, json_filename)
+
+mfcc, labels = load_dataset_from_json(json_filename)
